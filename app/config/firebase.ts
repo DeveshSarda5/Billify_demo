9408c83146422d65139b31442f0725fd1377bf41
Kapill56 <231210056@nitdelhi.ac.in>
2026-03-03 10:57:27 +0530

    Remove hardcoded Firebase API key and use env variables

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -5,20 +5,17 @@ import {
 } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// 🔥 Your REAL Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyA1DH9Ucen4Gxz7Z9lrcXu0HWDAucHjBXo",
-  authDomain: "billify-37eba.firebaseapp.com",
-  projectId: "billify-37eba",
-  storageBucket: "billify-37eba.firebasestorage.app",
-  messagingSenderId: "142514767464",
-  appId: "1:142514767464:web:f04ee859395cc7eb941ddd",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth with persistence
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
